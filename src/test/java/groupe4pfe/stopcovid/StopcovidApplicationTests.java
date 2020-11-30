eda721946509ce20a2de435d15eee2ea5bbed402
mustapha961 <49158614+mustapha961@users.noreply.github.com>
2020-11-30 21:52:19 +0100

    feat: adding simple test

--- src/test/java/groupe4pfe/stopcovid/StopcovidApplicationTests.java
+++ src/test/java/groupe4pfe/stopcovid/StopcovidApplicationTests.java
@@ -3,11 +3,15 @@ package groupe4pfe.stopcovid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertTrue;
+
+
 @SpringBootTest
 class StopcovidApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue("true",true);
     }
 
 }
