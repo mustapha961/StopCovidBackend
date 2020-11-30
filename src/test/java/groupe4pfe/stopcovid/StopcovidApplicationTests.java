89af202a5001029a004184bdcd861fdd865175f6
mustapha961 <49158614+mustapha961@users.noreply.github.com>
2020-11-30 22:41:17 +0100

    fix: tests

--- src/test/java/groupe4pfe/stopcovid/StopcovidApplicationTests.java
+++ src/test/java/groupe4pfe/stopcovid/StopcovidApplicationTests.java
@@ -3,8 +3,7 @@ package groupe4pfe.stopcovid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
