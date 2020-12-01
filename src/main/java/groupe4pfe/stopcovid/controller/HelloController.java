3e00aeb02844cc12b4e5e35b6ef2b8360e53f77a
mustapha961 <49158614+mustapha961@users.noreply.github.com>
2020-12-01 22:32:39 +0100

    test: variables d'environnements

--- src/main/java/groupe4pfe/stopcovid/controller/HelloController.java
+++ src/main/java/groupe4pfe/stopcovid/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -22,6 +23,13 @@ public class HelloController {
     @CrossOrigin
     public String test2(){
         return "Hello jeune citoyen";
+    }
+
+    @GetMapping("/env")
+    @CrossOrigin
+    public String test3(){
+        Map<String, String> env = System.getenv();
+        return env.get("env")+" ENVIRONMENT";
     }
 
 }
