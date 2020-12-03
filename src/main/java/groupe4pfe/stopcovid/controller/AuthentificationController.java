29da8c4ddf3f46d4615e44d59d97bdf0aea0f6c8
mustapha961 <49158614+mustapha961@users.noreply.github.com>
2020-12-03 12:59:37 +0100

    feat : ajout des class pour le modéle

---
+++ src/main/java/groupe4pfe/stopcovid/controller/AuthentificationController.java
@@ -0,0 +1,12 @@
+package groupe4pfe.stopcovid.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthentificationController {
+
+    public ResponseEntity<String> signupMedecin(){
+        return null;
+    }
+}
